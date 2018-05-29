a256c5ca780b2382c55348dc1170efc7977c52a1
manis6062 <manis6062@gmail.com>
2018-05-30 00:52:42 +0545

    Removed unwanted files

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -18,48 +18,6 @@ return [
 
 	"error" 				=> "Error",
 
-	"expiredwithlimit"		=> "Sorry your voucher is no longer valid",
-
-	"voucherpaid" 			=> "Thanks for your payment by Voucvher",
-
-	"feedbackinserted" 		=> "Feedback inserted",
-
-	"feedbackalreadyinserted" 	=> "Feedback has already inserted",
-
-	"insertfeedback" 	=> "Please insert feedback for your previous reservation",	
-
-	"nomorephotos" => "You have already upload 16 photos. You cannot add more",
-
-	"car_added" => "Car added successfully.",
-
-	"car_edited" => "Car edited successfully.",
-
-	"invalid_date_range" => "Invalid date range",
-
-	"you_have_selected_past_date" => "you have selected past date",
-
-	"photo_is_required" => "Please select photo to upload",
-
-	"calender_update_success" => "Successfully Updated",
-
-	"payment_initiated" => "Payment initiated for :booking_name. Thanks",
-
-	"ccresultnotfound" => "Credit Card not found",
-
-	"car_reapplied" => "Reapplied Car For Approval",
-
-	'message_error' => "The car you are trying to book has already been booked for the selected range",
-
-	'profile_pic_updated' => 'The profile picture has been updated of the company user. Please approve from the RCC admin panel.' ,
-
-	'car_pic_updated' => 'Car Details Updated .The car pic is under reviewed.' ,
-
-	'car_pic_updated_admin' => 'The car photo has been updated of the company user. Please approve from the RCC admin panel.' ,
-
-     'user_profile_pic_updated' => 'Profile Updated . The profile pic is under reviewed.' ,
-
-     "car_pic_edited" => "Car picture updated successfully.",
-
 
 ];
 
--- routes/api.php
+++ routes/api.php
@@ -14,136 +14,29 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 });
 
 
 
 # the api routes version 1
 Route::group(array('namespace' => 'Api' ), function() {
-
-
 	Route::post('users/fblogin', 'UsersController@fblogin');
-    Route::post('room/detail','RoomsController@detail');
-    Route::post('jagga/detail','JaggasController@detail');
+	Route::post('room/detail','RoomsController@detail');
+	Route::post('jagga/detail','JaggasController@detail');
 
-
-
-	Route::post('users/reset-password', 'UsersController@resetPassword');
-	Route::post('users/reset-password-process', 'UsersController@resetPasswordProcess');
-
-	Route::post('users/detail', 'UsersController@userDetail');
-	
-	Route::post('user/getcurrentuser', 'UsersController@getCurrentUser');
-
-	
-
-
-	Route::post('cars/search','CarsController@search');	
-	Route::post('cars/detail','CarsController@detail');
-	Route::post('cars/review','CarsController@carReview');
-	Route::post('cars/reviewofuser','CarsController@carReviewOfUser');
-	
-	Route::post('cars/calender','CarsController@getCarCalender');
-	Route::post('cars/reapply-car-listing','CarsController@reapplyCarListing');
-	
-
-	Route::post('vehicletype','VehicleTypeController@index');
-
-	Route::post('make','MakeController@index');
-
-	Route::post('models','ModelsController@index');
-
-	Route::post('features','CarFeaturesController@index');
-
-	Route::post('countries','CountryController@index');
-
-	Route::post('setting','SettingController@index');
-
-	//Route::post('setting','SettingController@index');
-
-	Route::post('cms/get-page','CmsController@getPage');
-
-	Route::post('estimated-valuesetAppSessions','EstimatedValuesController@index');
-	Route::post('cars/delivery-fee-list','CarsController@getDeliveryPricingList');
-	Route::post('cars/get-car-photos','CarsController@getCarPhotos');
-
-	Route::post('banks/get-bank-list', 'BanksController@index');
-
-	//Route::post('bookings/edit-booking/{booking}','BookingController@editBookingProcess');
-	//http://192.168.0.73/car_rent/public/api/cms/get-page
 });
 
 
 
 # all routes which requires accesstoken
 Route::group(array('middleware' =>['accesstokenchecker'], 'namespace' => 'Api' ), function() {
+	Route::post('users/logout', 'UsersController@logout');
+	Route::post('room/add','RoomsController@addRoom');
+	Route::post('room/update-room','RoomsController@updateRoom');
+	Route::post('jagga/add','JaggasController@addJagga');
+	Route::post('jagga/update-jagga','JaggasController@updateJagga');
+	Route::post('room/delete','RoomsController@deleteRoom');
+	Route::post('jagga/delete','JaggasController@deleteJagga');
 
-
-
-     Route::post('users/logout', 'UsersController@logout');
-     Route::post('room/add','RoomsController@addRoom');
-      Route::post('room/update-room','RoomsController@updateRoom');
-     Route::post('jagga/add','JaggasController@addJagga');
-     Route::post('jagga/update-jagga','JaggasController@updateJagga');
-     	Route::post('room/delete','RoomsController@deleteRoom');
-     	Route::post('jagga/delete','JaggasController@deleteJagga');
-     	
-
-
-	
-	Route::post('users/update-card-information', 'UserCardController@save');
-	Route::post('users/update-card-inform', 'UserCardController@inform');
-	
-	Route::post('booking/get-booking-price','BookingController@getBookingPrice');
-	Route::post('booking','BookingController@book');
-	Route::post('booking/my-rentals','BookingController@myRentals');
-	Route::post('booking/my-rental-details','BookingController@myRentalDetails');
-	Route::post('booking/my-car-rentals','BookingController@myCarRentals');
-	Route::post('booking/my-car-rental-details','BookingController@myCarRentalDetails');
-	Route::post('booking/cancel-booking','BookingController@cancelBooking');
-	Route::post('booking/reject-booking','BookingController@rejectBooking');
-	Route::post('booking/reject-booking-with-message', 'BookingController@rejectBookingwithMessage');
-
-	Route::post('booking/approve-booking','BookingController@approveBooking');
-	Route::post('booking/booking-message', 'BookingController@bookingMessage');
-
-
-	Route::post('review/add-review','ReviewsController@addReview');
-
-	Route::post('users/verify-licence', 'UsersController@verifyLicence');
-	Route::post('users/verify-mobile', 'UsersController@verifyMobile');
-	Route::post('users/verify-mobile-code', 'UsersController@verifyMobileCode');
-	Route::post('users/update-profile', 'UsersController@editProfile');
-	
-	Route::post('users/get-card-info', 'UserCardController@getPaymentInfo');
-	Route::post('users/update-deposite-account', 'EarningAccountController@save');
-	Route::post('users/get-deposite-account-info', 'EarningAccountController@getDepositeAccountInfo');
-	Route::post('users/notification', 'NotificationController@getNotification');
-	Route::post('users/get-unread-notification-count', 'NotificationController@getUnreadNotificationCount');
-	Route::post('users/read-notification', 'NotificationController@readNotification');
-	Route::post('users/message', 'MessageController@getMessage');
-	Route::post('users/send-message', 'MessageController@sendMessage');
-	Route::post('users/read-message', 'MessageController@readMessage');
-	Route::post('users/change-language', 'UsersController@changeLanguage');
-	Route::post('users/change-password', 'UsersController@changePassword');
-	Route::post('users/feedback', 'UsersController@feedback');
-
-	
-	Route::post('cars/update-car','CarsController@updateCar');
-	Route::post('cars/delete-car-photos','CarsController@deleteCarPhotos');
-	Route::post('cars/change-status','CarsController@changeStatus');
-	Route::post('cars/update-availability','CarsController@updateAvailability');
-	
-	Route::post('cars/update-delivery-option','CarsController@updateCarDeliveryOption');
-	Route::post('cars/update-mileage-option','CarsController@updateMileageOption');
-	Route::post('cars/add-car-photos','CarsController@addCarPhoto');
-
-	Route::post('cars/get-instant-booking-status','CarsController@getCardAndInstantBookingStatus');
-	Route::post('cars/update-instant-booking-status','CarsController@updateInstantBookingStatus');
-   Route::post('users/checkLicenceStatus', 'UsersController@checkLicenceStatus');
-   Route::post('users/checkMobileStatus', 'UsersController@checkMobileStatus');
-
-	
-	
 });
